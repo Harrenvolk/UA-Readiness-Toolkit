e4f400fac47c2c5a1d840119d390cba52b01368e
Sriram2001 <sriramudupi19@gmail.com>
2021-10-14 00:52:46 +0530

    fix (Version-1): Minor bug fixes

--- src/browser_interactor/main.py
+++ src/browser_interactor/main.py
@@ -13,31 +13,30 @@ from PIL import Image
 def generate_screenshot(list_of_domains, list_of_language_codes):
     DELAY = 3
     list_of_image_files = []
+    browser_driver_path = r"{}".format(os.environ.get("BROWSER_DRIVER_PATH"))
+    driver = webdriver.Chrome(executable_path=browser_driver_path)
+    driver.maximize_window()
     for (domain, language_code) in zip(list_of_domains, list_of_language_codes):    
         with mss() as sct:
-            browser_driver_path = "r" + "'" + os.environ.get("BROWSER_DRIVER_PATH") + "'"
-            driver = webdriver.Chrome(executable_path=browser_driver_path)
-            driver.maximize_window()
             try:
-                driver.get(domain)
-            except WebDriverException:
-                print("Failed to load website.")
-            try:
+                driver.get("https://"+domain)
                 default_html_element = WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.TAG_NAME, 'html')))
+                print("\n\n\n\n ++++++++++++++++++++++++++ \n\n\n\n")
+                print(driver.current_url)
+                print("\n\n\n\n ++++++++++++++++++++++++++ \n\n\n\n")
+                screenshot_filename = language_code + ".png"
+                sct.shot(output = screenshot_filename)
+                screenshot_filename = "./" + screenshot_filename
+                list_of_image_files.append(screenshot_filename)
             except TimeoutException:
                 print("Timeout!")
-            print("\n\n\n\n ++++++++++++++++++++++++++ \n\n\n\n")
-            print(driver.current_url)
-            print("\n\n\n\n ++++++++++++++++++++++++++ \n\n\n\n")
-            screenshot_filename = language_code + ".png"
-            sct.shot(output = screenshot_filename)
-            screenshot_filename = "./" + screenshot_filename
-            list_of_image_files.append(screenshot_filename)
+            except WebDriverException:
+                print("Failed to load website., {}".format(domain))
     driver.quit()
     return list_of_image_files
 
 def test_ua_readiness(list_of_image_files, list_of_language_codes):
-    tesseract_path = "r" + "'" + os.environ.get("TESSERACT_PATH") + "'"
+    tesseract_path = r"{}".format(os.environ.get("TESSERACT_PATH"))
     pytesseract.pytesseract.tesseract_cmd = tesseract_path
     for (image, language_code) in zip(list_of_image_files, list_of_language_codes):
        text=pytesseract.image_to_string(Image.open(image), lang=language_code)
@@ -47,7 +46,8 @@ def test_ua_readiness(list_of_image_files, list_of_language_codes):
 
 if __name__ == "__main__":
     load_dotenv()
-    list_of_domains = ["համընդհանուր-ընկալում-թեստ.հայ","универсальное-принятие-тест.москва","सार्वभौमिक-स्र्ीकृति-परीक्षण.संगठन"]
-    list_of_language_codes = ["hye","rus","hi"]
+    list_of_domains = ["համընդհանուր-ընկալում-թեստ.հայ","универсальное-принятие-тест.москва","सार्वभौमिक-स्वीकृति-परीक्षण.संगठन"]
+    list_of_language_codes = ["hye","rus","hin"]
+    print(list_of_domains)
     list_of_image_files = generate_screenshot(list_of_domains, list_of_language_codes)
     test_ua_readiness(list_of_image_files, list_of_language_codes)
