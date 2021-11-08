231555239b2bd19f5984434983c85c0429d42c88
Sriram2001 <sriramudupi19@gmail.com>
2021-11-08 23:26:25 +0530

    Add web interface

---
+++ app/static/test_urls.js
@@ -0,0 +1,41 @@
+$('.testcase .edit').click(function(){
+    var idx=$(this).attr('id').split('-')[1];
+    var href = $(this).prop('href'); 
+    if(!href){
+        $('#inp-'+idx).prop('disabled', false);
+        $(this).prop('href', '/update_test_url/'+idx);
+        $(this).removeClass('btn-outline-info');
+        $(this).addClass('btn-outline-success');
+        $(this).text("Submit");
+    } else {
+        $.ajax({
+            url: href,
+            type: "POST",
+            data: {
+                new_url: $('#inp-'+idx).val()
+            },
+            success: function(msg){
+                location.reload();
+            },
+            fail: function(xhr, textStatus, errorThrown){
+                alert("There was an unexpected error! Please try again.");
+            }
+        })
+    }
+    return false;
+})
+
+$('.testcase .delete').click(function(){
+    var href = $(this).prop('href'); 
+    $.ajax({
+        url: href,
+        type: "POST",
+        success: function(msg){
+            location.reload();
+        },
+        fail: function(xhr, textStatus, errorThrown){
+            alert("There was an unexpected error! Please try again.");
+        }
+    })
+    return false;
+})
---
+++ app/views.py
@@ -0,0 +1,57 @@
+from app import app
+from flask import render_template, request, redirect, jsonify
+import json
+
+@app.route("/")
+def index():
+    return render_template("test.html")
+
+@app.route("/config-testcases/")
+def config_testcases():
+    with open('./.config.json', 'r') as f:
+        data = json.load(f)
+        tests = data['tests']
+        for id,t in enumerate(tests):
+            t['id']=str(id)
+        return render_template("test_urls.html", tests=tests)
+    return "Error!"
+
+@app.route("/config-browsers/")
+def config_browsers():
+    return render_template("test_browsers.html")
+
+@app.route('/update_test_url/<int:id>', methods=["POST"])
+def update_test_url(id):
+    new_data = request.form['new_url']
+    with open('./.config.json', 'r+') as f:
+        data = json.load(f)
+        data['tests'][id]['url']=new_data
+        f.seek(0)
+        f.truncate()
+        json.dump(data, f)
+        resp=jsonify(success=True)
+        return resp
+
+@app.route('/delete_test_url/<int:id>', methods=['POST'])
+def delete_test_url(id):
+    with open('./.config.json', 'r+') as f:
+        data = json.load(f)
+        data['tests'].pop(id)
+        f.seek(0)
+        f.truncate()
+        json.dump(data, f)
+        resp=jsonify(success=True)
+        return resp
+
+@app.route('/add_test_url/', methods=['POST'])
+def add_test_url():
+    new_tc=request.form['new_tc']
+    with open('./.config.json', 'r+') as f:
+        data = json.load(f)
+        data['tests'].append({"url": new_tc})
+        f.seek(0)
+        f.truncate()
+        json.dump(data, f)
+        return redirect('/config-testcases/')
+
+
